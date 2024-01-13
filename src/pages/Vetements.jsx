c082e849ac2518ae4c424133602c5712b800cd46
Pi4a <piralith@outlook.fr>
2024-01-13 18:33:30 +0100

    bugfixes

--- src/pages/Vetements.jsx
+++ src/pages/Vetements.jsx
@@ -5,7 +5,7 @@ import "./vetements.css"
 function Vetements() {
   return (
     <div className="comingsoon"><img src="https://www.rollingstone.com/wp-content/uploads/2023/11/New_Vital_Seamless_Hero_Image.jpeg?crop=75px%2C0px%2C2250px%2C1500px&resize=1581%2C1054"></img>
-    <h1>BIENTOT DISPONIBLE</h1></div>
+    <h1 className='bientot'>BIENTOT DISPONIBLE</h1></div>
   )
 }
 
